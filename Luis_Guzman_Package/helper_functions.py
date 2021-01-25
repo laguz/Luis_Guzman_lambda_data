56dd89adc040276433e0e67b7b4df4094fed382f
Luis Alberto Guzman Zorrilla <laguz3@gmail.com>
2021-01-25 02:19:38 -0500

    Finished Homework 4

--- Luis_Guzman_Package/helper_functions.py
+++ Luis_Guzman_Package/helper_functions.py
@@ -3,6 +3,7 @@
 # accessing libraries through pipenv
 import pandas as pd
 import numpy as np
+import unittest
 
 
 def null_count(df):
@@ -27,3 +28,18 @@ def abbr_2_st(state_series, abbr_2_st=True):
     for state in state_series:
         abb = print(abbreviation[state])
         return abb
+
+
+class vehicle():
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_car_make(self):
+    self.assertEqual(lulu.make, 'Toyota')
+    self.assertEqual(lulu.model, 'Model3')
